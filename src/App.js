73eec888c8b612742611c98ca67fabe297e699f1
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-12-29 22:55:55 +0600

    style products component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import Items from '../src/products.json'
 import './App.css';
+import Products from './components/Products/Products';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-     
+    {
+      Items.map((item)=><Products
+      item={item}
+      key={item.id}
+      ></Products>)
+    }
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Products.css'
 
-const Products = () => {
+const Products = (props) => {
+   const {title,price,rating,description,img} = props.item;
     return (
-        <div>
-            
+        <div className='card-body'>
+            <img src={img} alt="" height="300px" width="300px" />
+            <h1>{title}</h1>
+            <h5>Price:{price}</h5>
+            <h6>Rating:{rating}</h6>
+            <p><i>{description}</i></p>
         </div>
     );
 };
